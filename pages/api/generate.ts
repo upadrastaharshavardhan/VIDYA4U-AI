fe16d6269148049b9892a07df9c70b01bf04e6bd
Harsha Vardhan upadrasta <62492737+upadrastaharshavardhan@users.noreply.github.com>
2024-04-17 11:53:42 +0530

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,4 +1,4 @@
-import { NextRequest, Response } from "next";
+import type { NextRequest } from "next/server";
 import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
 
 if (!process.env.OPENAI_API_KEY) {
